441f1b2e44f3d38e268457038d08b7a1fde16dc0
Nawal <119487143+NawalKavi@users.noreply.github.com>
2023-11-21 10:54:26 +0000

    WORKING ANIMATION LET'S GOOOOO (for now)

--- main.py
+++ main.py
@@ -19,7 +19,15 @@ auDictionary = {  # Dictionary storing all the frames of each individual Among U
         1: pygame.transform.scale(pygame.image.load("Assets/Sprites/Purple/Frame 1.png"), (60, 70)).convert_alpha(),  # Converts the image to something Pygame can work with easier.
         2: pygame.transform.scale(pygame.image.load("Assets/Sprites/Purple/Frame 2.png"), (60, 70)).convert_alpha(),
         3: pygame.transform.scale(pygame.image.load("Assets/Sprites/Purple/Frame 3.png"), (60, 70)).convert_alpha(),
-        4: pygame.transform.scale(pygame.image.load("Assets/Sprites/Purple/Frame 4.png"), (60, 70)).convert_alpha()
+        4: pygame.transform.scale(pygame.image.load("Assets/Sprites/Purple/Frame 4.png"), (60, 70)).convert_alpha(),
+        "currentFrame": 1
+    },
+    "red": {
+        1: pygame.transform.scale(pygame.image.load("Assets/Sprites/Red/Frame 1.png"), (60, 70)).convert_alpha(),
+        2: pygame.transform.scale(pygame.image.load("Assets/Sprites/Red/Frame 2.png"), (60, 70)).convert_alpha(),
+        3: pygame.transform.scale(pygame.image.load("Assets/Sprites/Red/Frame 3.png"), (60, 70)).convert_alpha(),
+        4: pygame.transform.scale(pygame.image.load("Assets/Sprites/Red/Frame 4.png"), (60, 70)).convert_alpha(),
+        "currentFrame": 1
     }
 }
 
@@ -28,12 +36,11 @@ purpleAU_xPos = -70
 purpleAU_yPos = 700
 
 # Rectangles.
-purpleAU_rect = auDictionary["purple"][1].get_rect(midbottom = (purpleAU_xPos, purpleAU_yPos))
+purpleAU_rect = auDictionary["purple"][1].get_rect(midbottom=(purpleAU_xPos, purpleAU_yPos))
 
 # Surfaces.
 menuTempBackground = pygame.Surface((1200, 800))
 menuText = menuFont.render("SUSUSUSUS", False, "White")
-
 
 '''
 class MenuAU(pygame.sprite.Sprite):
@@ -46,7 +53,7 @@ class MenuAU(pygame.sprite.Sprite):
 MenuAU_sprite = MenuAU()
 '''
 
-
+counter = 0
 # Game loop.
 while True:
     for event in pygame.event.get():  # Retrieves all events running.
@@ -61,7 +68,18 @@ while True:
     purpleAU_rect.left += 2
     if purpleAU_rect.left > 1200:
         purpleAU_rect.left = -70
-    screen.blit(auDictionary["purple"][1], purpleAU_rect)
+    screen.blit(auDictionary["purple"][auDictionary["purple"]["currentFrame"]], purpleAU_rect)
+
+    if counter < 10:
+        counter += 1
+    else:
+        counter = 0
+        for image in auDictionary:
+            image = auDictionary[image]
+            if image["currentFrame"] < len(image) - 1:
+                image["currentFrame"] += 1
+            else:
+                image["currentFrame"] = 1
 
     pygame.display.update()
     clock.tick(60)  # Sets the framerate; here, 60fps has been set as the target.
